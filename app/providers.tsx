0511c5e2117c0e4031a189613e67647be545d7ae
SAEED.PANAHI <panahi.projects@gmail.com>
2025-04-26 15:35:07 +0330

    add default provider: tanstack-query, add default toast

---
+++ app/providers.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactNode, useState } from "react";
+import { Toaster } from "react-hot-toast";
+
+export function Providers({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster position="top-right" />
+    </QueryClientProvider>
+  );
+}
